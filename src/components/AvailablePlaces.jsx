96f736ad42591df542ee042ee538c7748e34aab1
Amr <frank1276smith@gmail.com>
2023-12-31 18:08:41 +0200

    adding try-catch block to handle HTTP errors. adding a new component which is error.jsx. adding a new useState (error state) which you use to show the potential error on the screen

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,21 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePLaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
-  fetch("http://localhost:3000/places")
-    .then((response) => {
-      return response.json();
-    })
-    .then((resData) => {
-      setAvailablePlaces(resData.places);
-    });
+  /* useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []); */
+
+  // Using async await function to access api
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places :(");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message ||
+            "Could not fetch places, Please try again later :(",
+        });
+      }
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePLaces}
+      isLoading={isFetching}
+      loadingText="Fetching Data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
