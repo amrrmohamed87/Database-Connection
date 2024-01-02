65e7ef6ac96a8fdcad9caf615e23f4445b3a7443
Amr <frank1276smith@gmail.com>
2024-01-02 14:51:22 +0200

    deploying the project

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,6 +3,13 @@ export default function Error({ title, message, onConfirm }) {
     <div className="error">
       <h2>{title}</h2>
       <p>{message}</p>
+      {onConfirm && (
+        <div id="confirmation-action">
+          <button onClick={onConfirm} className="button">
+            Okay
+          </button>
+        </div>
+      )}
     </div>
   );
 }
